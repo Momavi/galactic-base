d7e3261c605082e64ad39af51e8452e28c3e29cc
Momavi <68790545+Momavi@users.noreply.github.com>
2021-09-24 02:22:19 +0300

    Update Paginator.vue

--- src/components/Paginator.vue
+++ src/components/Paginator.vue
@@ -3,20 +3,29 @@
     <div
       class="paginator__btn-prev paginator__btn"
       v-bind:class="{ active: STARSHIPS.previous }"
-      @click="this.GET_STARSHIPS_PREV()"
+      @click="
+        this.GET_STARSHIPS_URL({ link: STARSHIPS.previous, atribute: 'prev' })
+      "
     >
       Пред
     </div>
-    <ul>
-      <li>1</li>
-      <li>2</li>
-      <li>3</li>
-      <li>4</li>
+    <ul class="paginator__list">
+      <li
+        class="paginator__list-li"
+        v-for="n in 4"
+        :key="n"
+        v-bind:class="{ active: CURRENTPAGE === n }"
+        @click="this.GET_STARSHIPS_NUMBER(n)"
+      >
+        {{ n }}
+      </li>
     </ul>
     <div
       class="paginator__btn-next paginator__btn"
       v-bind:class="{ active: STARSHIPS.next }"
-      @click="this.GET_STARSHIPS_NEXT()"
+      @click="
+        this.GET_STARSHIPS_URL({ link: STARSHIPS.next, atribute: 'next' })
+      "
     >
       След
     </div>
@@ -32,20 +41,46 @@ export default {
     STARSHIPS() {
       return this.$store.getters.STARSHIPS;
     },
+    CURRENTPAGE() {
+      return this.$store.getters.CURRENTPAGE;
+    },
   },
   methods: {
-    ...mapActions(["GET_STARSHIPS_PREV"]),
-    ...mapActions(["GET_STARSHIPS_NEXT"]),
+    ...mapActions(["GET_STARSHIPS_URL"]),
+    ...mapActions(["GET_STARSHIPS_NUMBER"]),
   },
 };
 </script>
 
 <style scoped lang="scss">
+.active {
+  color: #00ff8ccb;
+  text-decoration: underline;
+}
 .paginator {
   display: flex;
   align-items: center;
   justify-content: center;
   align-content: center;
+  &__list {
+    display: flex;
+    padding: 0;
+    &-li {
+      margin-right: 10px;
+      list-style-type: none;
+      padding: 5px;
+      border: 2px solid transparent;
+      border-radius: 25px;
+      cursor: pointer;
+      &:last-child {
+        margin-right: 0;
+      }
+      &:hover {
+        border: 2px solid #00ff8ccb;
+        background-color: #093a23cb;
+      }
+    }
+  }
   &__btn {
     background-color: #0d2e1f67;
     border: 2px solid #013820b6;
@@ -53,9 +88,10 @@ export default {
     border-radius: 15px;
     margin: 0;
     padding: 6px 15px;
+    text-decoration: none;
     user-select: none;
+    cursor: pointer;
     transition: all 0.3s;
-
     &-prev {
       margin-right: 30px;
     }
